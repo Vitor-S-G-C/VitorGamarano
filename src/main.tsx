d166348fb42b1e497a74ee030d8c398e083032ab
Vitor Gamarano <vitorgamarano1@gmail.com>
2026-06-11 13:54:49 -0300

    Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
